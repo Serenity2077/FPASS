da115c83dd40e2d01dc16c10bd26afbc9027f9c4
Serenity2077 <87119055+Serenity2077@users.noreply.github.com>
2023-09-05 08:54:10 +0000

    Deploying to gh-pages from @ Serenity2077/FPASS@eeae67244f88fd1e506c6cba84977cb78e39d2a0 🚀

---
+++ precache-manifest.fbb3a0db3285afe14fb66dc80202db47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f5838228ba6640913532be7774671f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1414ecc9a7e8239bce71",
+    "url": "/static/css/2.e4fb8d07.chunk.css"
+  },
+  {
+    "revision": "1414ecc9a7e8239bce71",
+    "url": "/static/js/2.b0dc5f0c.chunk.js"
+  },
+  {
+    "revision": "af30ab4ecc10080249f21b554426b8eb",
+    "url": "/static/js/2.b0dc5f0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ba4d44e352148ad0d1b",
+    "url": "/static/js/main.914bc933.chunk.js"
+  },
+  {
+    "revision": "7c899614f8c81cd88e0b",
+    "url": "/static/js/runtime-main.55d7ce28.js"
+  }
+]);
